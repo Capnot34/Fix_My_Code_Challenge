95c5dc39e1a4fce989a450854478dcab540b1115
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-10-28 15:49:26 +0100

    Update delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -47,7 +47,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-		free (*head);
 		*head = saved_head;
 	}
 	return (1);
